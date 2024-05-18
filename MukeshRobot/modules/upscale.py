58f9b03efee2ba1f4ca310a6cc526eb72d90bfcb
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2024-05-18 20:39:41 +0530

    Update upscale.py

--- MukeshRobot/modules/upscale.py
+++ MukeshRobot/modules/upscale.py
@@ -44,7 +44,7 @@ async def upscale_image(client, message):
             await client.send_document(
                 message.chat.id,
                 document=upscaled_file_path,
-                caption=f"✦ **ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ➛** [˹ 𝗦𝙴𝙽𝙾𝚁𝙸𝚃𝙰 ✘ 𝗥𝙾𝙱𝙾 ˼](https://t.me/StrangerSuperbot)\n\n✦ **ᴄʀᴇᴅɪᴛs** ➛ [sʜɪᴠᴀɴsʜ-xᴅ](https://t.me/SHIVANSH474)",
+                caption=f"✦ **ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ➛** [˹ 𝗦ʜᴜʙʜ ✘ 𝗥𝙾𝙱𝙾 ˼](https://t.me/Snehaxprobot)\n\n✦ **ᴄʀᴇᴅɪᴛs** ➛ [sʜᴜʙʜ](https://t.me/Mrpasserby_1227)",
             )
         else:
             await message.reply_text("✦ ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ɪᴛ.")
